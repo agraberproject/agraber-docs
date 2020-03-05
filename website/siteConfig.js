14bf7489b51ada78073a4e97154169ba2e026ebf
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2020-03-05 15:07:24 -0300

    Fix null docs on siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -44,8 +44,8 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
+    {doc: 'disclaimer', label: 'Docs'},
+    {doc: 'basic_info', label: 'Primeiros Passos'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
   ],

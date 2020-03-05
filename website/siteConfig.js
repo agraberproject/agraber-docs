f1991b9bbfebd6a6b50f81630560c88b59b768c6
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2020-03-05 14:40:56 -0300

    Atualiza siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -29,15 +29,15 @@ const users = [
 const siteConfig = {
   title: 'AGraber 30i', // Title for your website.
   tagline: 'Documentação e Guias',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://agraberproject.github.io', // Your website URL
+  baseUrl: '/agraber-docs/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'agraber-docs',
-  organizationName: 'agraber-30i',
+  projectName: 'agraberproject.github.io',
+  organizationName: 'agraberproject',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
